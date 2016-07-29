fa1b6eec344b882b45cfae640396e8cbf930801e
liarch29 <charliean19@gmail.com>
2016-07-28 19:41:52 -0700

    Add animation thing

--- Assets/Scripts/EnemySummon.cs
+++ Assets/Scripts/EnemySummon.cs
@@ -16,14 +16,19 @@ public class EnemySummon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
-		if (Time.time > FireTime) {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.RotateAround (transform.position, Vector3.left, -90 * Time.deltaTime);
+		}else if (Input.GetKey (KeyCode.D)) {
+			transform.RotateAround (Vector3.zero, Vector3.up, 90 * Time.deltaTime);
+		}
+		//transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
+		/*if (Time.time > FireTime) {
 			//Instantiate (Spear, transform.position, transform.rotation);
 			//Instantiate (CannonBall, transform.position, transform.rotation);
 			Instantiate (BouncyCube, transform.position, transform.rotation);
 			//Instantiate (Rocket, transform.position, transform.rotation);
 			FireTime = Time.time + delay;
-		}
+		}*/
 	}
 
 }
