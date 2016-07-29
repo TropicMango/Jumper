003ef2b05e78a384440358f41db04164f99fe65d
liarch29 <charliean19@gmail.com>
2016-07-28 20:45:43 -0700

    Camera

---
+++ Assets/Scripts/LockScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockScreen : MonoBehaviour {
+
+	public GameObject Player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3(Player.transform.position.x,transform.position.y-offset.y,Player.transform.position.z) + offset;
+	}
+}
