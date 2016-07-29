ed43c3bbf3922f434a768c4dba20fe4c3f3941ae
liarch29 <charliean19@gmail.com>
2016-07-28 20:22:37 -0700

    player moves now?

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -2,14 +2,28 @@
 using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
+	private Vector3 pivot;
+	public int speed = 250;
+	public double degrees;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		pivot = new Vector3 (transform.position.x + 2.5F, transform.position.y, transform.position.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKey (KeyCode.A)) {
+			transform.RotateAround (pivot, Vector3.forward, -speed * Time.deltaTime);
+			if (degrees>180) {
+				degrees = 0;
+				transform.position = new Vector3 (pivot.x + 2.5F, pivot.y, pivot.z);
+				transform.rotation = Quaternion.Euler(new Vector3(0,0,0)); 
+				pivot = new Vector3 (transform.position.x + 2.5F, transform.position.y, transform.position.z);
+			}
+			degrees += speed * Time.deltaTime;
+			Debug.Log (degrees);
+		}
 	}
 }
