a67193e2ab9e5e3edf1ef97e0c546a2868cc584f
Justin Yu <justin.v.yu@gmail.com>
2016-07-28 21:01:16 -0700

    Add gestures

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -14,16 +14,31 @@ public class CharacterMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.A)) {
-			transform.RotateAround (pivot, Vector3.forward, -speed * Time.deltaTime);
-			if (degrees>180) {
-				degrees = 0;
-				transform.position = new Vector3 (pivot.x + 2.5F, pivot.y, pivot.z);
-				transform.rotation = Quaternion.Euler(new Vector3(0,0,0)); 
-				pivot = new Vector3 (transform.position.x + 2.5F, transform.position.y, transform.position.z);
-			}
-			degrees += speed * Time.deltaTime;
-			Debug.Log (degrees);
-		}
+        if (Input.touchCount == 1) {
+            if (Input.touches [0].phase == TouchPhase.Began || 
+                Input.touches [0].phase == TouchPhase.Stationary) {
+                transform.RotateAround (pivot, Vector3.forward, -speed * Time.deltaTime);
+                if (degrees>180) {
+                  degrees = 0;
+                  transform.position = new Vector3 (pivot.x + 2.5F, pivot.y, pivot.z);
+                  transform.rotation = Quaternion.Euler(new Vector3(0,0,0)); 
+                  pivot = new Vector3 (transform.position.x + 2.5F, transform.position.y, transform.position.z);
+                }
+                degrees += speed * Time.deltaTime;
+                Debug.Log (degrees);
+            }
+        }
+
+//		if (Input.GetKey (KeyCode.A)) {
+//			transform.RotateAround (pivot, Vector3.forward, -speed * Time.deltaTime);
+//			if (degrees>180) {
+//				degrees = 0;
+//				transform.position = new Vector3 (pivot.x + 2.5F, pivot.y, pivot.z);
+//				transform.rotation = Quaternion.Euler(new Vector3(0,0,0)); 
+//				pivot = new Vector3 (transform.position.x + 2.5F, transform.position.y, transform.position.z);
+//			}
+//			degrees += speed * Time.deltaTime;
+//			Debug.Log (degrees);
+//		}
 	}
 }
---
+++ Assets/Scripts/GestureRecognizer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GestureRecognizer : MonoBehaviour {
+
+    public float swipeThreshold = 1.0f;
+    private Vector2 swipeStart = Vector2.zero;
+    private Vector2 swipeEnd = Vector2.zero;
+    private bool swipeWasActive = false;
+
+    void Start () {
+
+    }
+
+    void Update () {
+        if (Input.touchCount == 1) {
+            ProcessSwipe ();
+        }
+    }
+
+    void ProcessSwipe() {
+        if (Input.touchCount != 1) {
+            return;
+        }
+
+        Touch touch = Input.touches [0];
+        if (touch.deltaPosition == Vector2.zero) {
+            return;
+        }
+
+        Vector2 speedVector = touch.deltaPosition * touch.deltaTime;
+        float speed = speedVector.magnitude;
+
+        bool swipeIsActive = (speed > swipeThreshold);
+
+        if (swipeIsActive) {
+            if (!swipeWasActive) {
+                swipeStart = touch.position;
+            }
+        } else {
+            if (swipeWasActive) {
+                swipeEnd = touch.position;
+                Debug.Log("Swipe Complete");
+            }
+        }
+
+        swipeWasActive = swipeIsActive;
+    }
+}
